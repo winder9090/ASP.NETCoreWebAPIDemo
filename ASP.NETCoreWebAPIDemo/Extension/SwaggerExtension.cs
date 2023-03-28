b2689fd1ef4304e1ec03ca93ee84802d8dd62714
winder9090 <winder9090@gmail.com>
2023-03-28 13:16:28 +0800

    1.增加SwaggerExtension用于Swagger配置 2.将Startup中Swagger相关配置移入SwaggerExtension

---
+++ ASP.NETCoreWebAPIDemo/Extension/SwaggerExtension.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace ASP.NETCoreWebAPIDemo.Extension
+{
+    public static class SwaggerExtension
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app"></param>
+        public static void UseSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger(c =>
+            {
+
+            });
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ASP.NETCoreWebAPIDemo v1"));
+        }
+
+        public static void AddSwaggerConfig(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "ASP.NET Core Web API Demo",
+                    Version = "v1",
+                    Description = "",
+                });
+            });
+        }
+    }
+}
--- ASP.NETCoreWebAPIDemo/Startup.cs
+++ ASP.NETCoreWebAPIDemo/Startup.cs
@@ -1,16 +1,9 @@
+using ASP.NETCoreWebAPIDemo.Extension;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ASP.NETCoreWebAPIDemo
 {
@@ -28,10 +21,11 @@ namespace ASP.NETCoreWebAPIDemo
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ASP.NETCoreWebAPIDemo", Version = "v1" });
-            });
+
+
+            #region Swagger
+            services.AddSwaggerConfig();
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,9 +34,11 @@ namespace ASP.NETCoreWebAPIDemo
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ASP.NETCoreWebAPIDemo v1"));
             }
+
+            #region Swagger
+            app.UseSwagger();
+            #endregion
 
             app.UseHttpsRedirection();
 
