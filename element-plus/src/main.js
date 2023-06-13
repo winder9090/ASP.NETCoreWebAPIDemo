40e42f9e90bc10473ca2cf269981a634f9f15456
winder9090 <winder9090@gmail.com>
2023-06-13 09:41:01 +0800

    添加 Element Plus

--- element-plus/src/main.js
+++ element-plus/src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import installElementPlus from './plugins/element'
+import locale from 'element-plus/lib/locale/lang/zh-cn'
+import { ElMessage } from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+installElementPlus(app)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.mount('#app')
