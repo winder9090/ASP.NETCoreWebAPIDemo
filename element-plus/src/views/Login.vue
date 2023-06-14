1dc23eb0d300022fc59808eb72b39bc1ae186660
winder9090 <winder9090@gmail.com>
2023-06-14 14:16:09 +0800

    增加tsconfig.json

--- element-plus/src/views/Login.vue
+++ element-plus/src/views/Login.vue
@@ -35,7 +35,7 @@
 <script>
     export default{
         setup (){
-             window.sessionStorage.clear()
+            window.sessionStorage.clear()
         },
         data() {
             return{
